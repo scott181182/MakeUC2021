67ba23c5762f7340250fa893cd6e3694f42a255b
Scott Fasone <scott181182@yahoo.com>
2021-10-09 15:21:27 -0400

    Initial code setup

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        "indent": [ "error", 4 ],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "quotes": ["error", "double", { "avoidEscape": false }],
+        "vue/html-indent": ["error", 4 ],
+        "vue/html-self-closing": [ "error", {
+            "html": {
+                "void": "always"
+            }
+        }],
+        "vue/script-indent": [ "error", 4 ]
+    }
+}
